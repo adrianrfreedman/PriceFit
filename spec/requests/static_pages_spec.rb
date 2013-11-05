d1f04f3a9887393df57e20186c9c5e37e047bb53
Adrian Freedman <adrianrfreedman@gmail.com>
2013-11-06 10:07:48 +1100

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading)    { 'PriceFit' }
+    let(:page_title) { '' }
+    
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+    let(:heading)    { 'About Us' }
+    let(:page_title) { 'About Us' }
+    
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Home"
+    click_link "Sign Up"
+    expect(page).to have_title(full_title('Sign Up'))
+    click_link "PriceFit"
+    expect(page).to have_title(full_title(''))
+  end
+end
